7b35011ad64b3f95c5d155b3e21955cbff996d13
Patrick Kaeding <pkaeding@launchdarkly.com>
2020-10-28 15:08:36 -0400

    added another code reference

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -9,6 +9,8 @@ public class App
 {
     public static void main( String[] args )
     {
+LDUser user = new LDUser("user@test.com");
+boolean showFeature = ldClient.boolVariation("practical-fresh-car-4310", user, false);
     	//Comment
         System.out.println( "Hello World!" );
     }
